f89929991d3c9e71b76a4598be1ac85ab93c618e
zegof <117827585+zegof@users.noreply.github.com>
2026-01-05 11:14:30 +0100

    update ensure_args docstring

--- src/core/funcs/ensure.py
+++ src/core/funcs/ensure.py
@@ -1,15 +1,16 @@
-from flask import jsonify
-from typing import Any
+from flask import jsonify, Response
+from typing import Any, Literal
 
-def ensure_args(d: dict[Any, Any], keys: list[str]):
-    """Returns a Response error if a key is not present in the dictionary.
-
-    Args:
-        d (dict): The dictionary to check.
-        keys (list[str]): A list of keys to check.
-
-    Returns:
-        Response | None: An error Response if a key is missing, else None
+def ensure_args(d: dict[Any, Any], keys: list[str]) -> (tuple[Response, Literal[400]] | None):
+    """
+    Returns a Response error if a key is not present in the dictionary.
+    
+    :param d: The dictionary to check.
+    :type d: dict[Any, Any]
+    :param keys: A list of keys to check.
+    :type keys: list[str]
+    :return: An error Response if a key is missing, else None
+    :rtype: tuple[Response, Literal[400]] | None
     """
     for key in keys:
         if not key in d:
